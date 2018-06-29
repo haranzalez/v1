e13b44c936804fbb3ef3cf170478fa7c69dbe460
Hans <haranzalez@gmail.com>
2018-06-29 10:44:07 -0500

    init setup

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,11 @@
+
 import { sync } from 'vuex-router-sync';
 import Vuetify from 'vuetify';
 
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from './store';
+import store from './store/index';
 
 Vue.config.productionTip = false;
 
